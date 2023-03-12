f7a8c4a334bb0b8fe48854b3657c536fc76769b3
Mihail-1212 <m.a.mokruschin@yandex.com>
2023-03-12 16:08:44 +0500

    Add locale and app name

--- acs_teachers_backend/apps/student/apps.py
+++ acs_teachers_backend/apps/student/apps.py
@@ -5,4 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class StudentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'student'
-    verbose_name = _('students')
+    verbose_name = _('Students application')
