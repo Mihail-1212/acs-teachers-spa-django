11c9ba46129f33998d418b597a23437b2138b970
Mihail-1212 <m.a.mokruschin@yandex.com>
2023-03-14 21:48:33 +0500

    Add api and routes for eduorg, special, discipline

--- acs_teachers_backend/apps/journal/api.py
+++ acs_teachers_backend/apps/journal/api.py
@@ -1,27 +1,76 @@
+from enum import unique
+from strenum import StrEnum
+
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import (
     IsAdminUser
 )
+from django.utils.translation import gettext_lazy as _
 
 from . import serializers
 from . import models
 
 
-class EduOrganizationViewSet(viewsets.ModelViewSet):
+@unique
+class ApiParams(StrEnum):
+    """
+    Api enum set params for viewset classes
+    """
+    EDU_ORGANIZATION_ID = "edu_organization_id"
+    SPECIALITY_ID = "speciality_id"
+
+
+class EduOrganizationViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.EduOrganization.objects.all()
     serializer_class = serializers.EduOrganizationSerializer
     lookup_field = 'id'
 
 
 class SpecialityViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = models.Speciality.objects.all()
     serializer_class = serializers.SpecialitySerializer
     lookup_field = 'id'
 
+    def get_queryset(self):
+        """
+        Get the list of items for this view
+        """
+        # Getting queryset
+        queryset = models.Speciality.objects.all()
+
+        # Get education organization id from URL GET params
+        edu_organization_id = self.request.query_params.get(ApiParams.EDU_ORGANIZATION_ID)
+        if edu_organization_id is not None:
+            if not edu_organization_id.isnumeric():
+                raise ValidationError(_("Education organization id should be a number value"))
+            queryset = queryset.filter(edu_organizations__id=edu_organization_id)
+
+        return queryset
+
 
 class DisciplineViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = models.Discipline.objects.all()
     serializer_class = serializers.DisciplineSerializer
     lookup_field = 'slug'
 
+    def get_queryset(self):
+        """
+        Get the list of items for this view
+        """
+        # Getting queryset
+        queryset = models.Discipline.objects.all()
 
+        # Get speciality id from URL GET params
+        speciality_id = self.request.query_params.get(ApiParams.SPECIALITY_ID)
+        if speciality_id is not None:
+            if not speciality_id.isnumeric():
+                raise ValidationError(_("Speciality id should be a number value"))
+            queryset = queryset.filter(specialities__id=speciality_id)
+
+        # Get education organization id from URL GET params
+        edu_organization_id = self.request.query_params.get(ApiParams.EDU_ORGANIZATION_ID)
+        if edu_organization_id is not None:
+            if not edu_organization_id.isnumeric():
+                raise ValidationError(_("Education organization id should be a number value"))
+            queryset = queryset.filter(specialities__edu_organizations__id=edu_organization_id)
+
+        return queryset
--- acs_teachers_backend/apps/journal/urls.py
+++ acs_teachers_backend/apps/journal/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path, include
 from rest_framework import routers
-from django.views.generic import RedirectView
-from django.urls import path
 
 from . import api
 
